e8138711ba68ff2a144b2b7734238feebda3d6fd
harrythrees <harrythrees@users.noreply.github.com>
2017-03-06 19:09:34 +0100

    Update app.js

--- www/js/build/app.js
+++ www/js/build/app.js
@@ -227,7 +227,7 @@ var App = React.createClass({displayName: "App",
 
 alert("ConsoleMsg5");
 
-React.render(
+ReactDOM.render(
   React.createElement('h1', {className: 'heading'}, 'Hello World'),
   document.getElementById('main')
 );
