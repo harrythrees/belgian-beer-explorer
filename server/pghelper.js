c21dfa144f4fa8ca843b9780c8e5076bbe2ebfdd
harrythrees <harrythrees@users.noreply.github.com>
2017-03-06 14:54:26 +0100

    Update pghelper.js

--- server/pghelper.js
+++ server/pghelper.js
@@ -15,6 +15,7 @@ exports.query = function (sql, values, singleItem, dontLog) {
     if (!dontLog) {
         console.log(sql, values);
     }
+    console.error(sql, values);
 
     var deferred = Q.defer();
 
