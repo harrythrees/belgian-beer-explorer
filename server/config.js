0b8131a282f93d2ece47f3e43c3af86944b34e02
harrythrees <harrythrees@users.noreply.github.com>
2017-03-06 14:38:31 +0100

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,5 +1,6 @@
 module.exports = {
 
     // databaseURL: process.env.DATABASE_URL || "postgres://ccoenraets@localhost/belgianbeers"
+    databaseURL: "postgres://dirnkexkkutapr:m-NVrNNkTL-BsLpwspI9m5fSmM@ec2-54-83-17-8.compute-1.amazonaws.com:5432/d9kn2a691gd1mp"
 
 };
